8ee5452fddb7f3918f7e931b7345252883edd97a
animerusya <rus.melanin@mail.ru>
2025-07-26 14:09:32 +0300

    Проект создан

---
+++ Yes No Game/Yes_No_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yes_No_GameApp.swift
+//  Yes No Game
+//
+//  Created by Руслан Меланин on 26.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Yes_No_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
